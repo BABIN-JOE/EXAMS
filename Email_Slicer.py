6b8dc930932791fc56a77b662924c3fce8e8205f
BABIN JOE <143618768+BABIN-JOE@users.noreply.github.com>
2023-11-03 10:37:44 +0530

    Delete Email_Slicer.py

--- Email_Slicer.py
+++
@@ -1,9 +0,0 @@
-# Get the email address from the user
-email = input("Enter an email address: ")
-
-# Split the email address into username and domain
-username, domain = email.split("@")
-
-# Print the results
-print(f"Username: {username}")
-print(f"Domain: {domain}")
