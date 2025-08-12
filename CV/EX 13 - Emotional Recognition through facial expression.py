f8a0a483e7fb68b12c5db6f9bf95e6b10009501a
BABIN JOE <babinjoe2945@gmail.com>
2025-08-12 21:15:34 +0530

    Update EX 13 - Emotional Recognition through facial expression.py

--- CV/EX 13 - Emotional Recognition through facial expression.py
+++ CV/EX 13 - Emotional Recognition through facial expression.py
@@ -1,1 +1,18 @@
+!pip install deepface
 
+
+import cv2
+import matplotlib.pyplot as plt
+from deepface import DeepFace
+from google.colab import files
+
+uploaded = files.upload()
+image_path = list(uploaded.keys())[0]
+
+img = cv2.imread(image_path)
+plt.imshow(img[:, :, ::-1])  # BGR to RGB for correct colors
+plt.axis('off')
+plt.show()
+
+result = DeepFace.analyze(img, actions=['emotion'])
+print(result)
